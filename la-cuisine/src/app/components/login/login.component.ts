62dcb37b3899f2f705a81e1686f1e33c115300a6
Bruno <bruno6244.bh@gmail.com>
2024-11-05 23:09:57 -0300

    :sparkles: feat: Enhance login component visuals and functionality

--- la-cuisine/src/app/components/login/login.component.ts
+++ la-cuisine/src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, signal, ViewChild } from '@angular/core';
 import { LoginService } from './login.service';
 import { CommonModule } from '@angular/common';
 
@@ -11,9 +11,56 @@ import { CommonModule } from '@angular/common';
 })
 
 
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
 
-  isVisible = this.loginService.isLoginVisible;
+  texts = {
+    heading: 'Novo por Aqui ?',
+    paragraph: 'Cadastre-se para gerenciar suas reservas, simples e rápido',
+    button: 'Cadastre-se'
+  };
 
-  constructor(public loginService: LoginService) {}
+  buttonClicked = signal(false);
+
+  @ViewChild('panelRef') panel!: ElementRef;
+  @ViewChild('signInRef') signIn!: ElementRef;
+  @ViewChild('signUpRef') signUp!: ElementRef;
+  @ViewChild('panelH2Ref') panelH2!: ElementRef;
+  @ViewChild('panelPRef') panelP!: ElementRef;
+  @ViewChild('panelButtonRef') panelButton!: ElementRef;
+
+
+  ngAfterViewInit(): void {
+    // Aqui você pode acessar os elementos DOM diretamente
+    console.log(this.panel.nativeElement); // Acesso ao elemento do painel
+    console.log(this.signIn.nativeElement); // Acesso ao elemento de login
+    console.log(this.signUp.nativeElement); // Acesso ao elemento de cadastro
+    console.log(this.panelH2.nativeElement); // Acesso ao elemento do painel
+    console.log(this.panelP.nativeElement); // Acesso ao elemento de login
+    console.log(this.panelButton.nativeElement); // Acesso ao elemento de cadastro
+
+  }
+
+  click() {
+    this.buttonClicked.set(true);
+    if (this.panel && this.signIn && this.signUp) {
+      this.panel.nativeElement.classList.toggle("togglepanel"); 
+      this.signIn.nativeElement.classList.toggle("hide");
+      this.signUp.nativeElement.classList.toggle("hide");
+
+      if (this.texts.heading === 'Novo por Aqui ?') {
+        this.texts.heading = 'Já tem uma conta ?';
+        this.texts.paragraph = 'Entre para gerenciar suas reservas, ou avaliar sua visita';
+        this.texts.button = 'Entrar';
+      } else {
+        this.texts.heading = 'Novo por Aqui ?';
+        this.texts.paragraph = 'Cadastre-se para gerenciar suas reservas, simples e rápido';
+        this.texts.button = 'Cadastre-se';
+      }
+  }
+}
+
+isVisible = this.loginService.isLoginVisible;
+
+constructor(public loginService: LoginService) {}
+
 }
